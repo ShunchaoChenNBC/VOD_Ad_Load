59551199a8f1e477a4d5cb111d0d31a00a6009f0
ShunchaoChenNBC <117398619+ShunchaoChenNBC@users.noreply.github.com>
2023-04-27 14:16:22 -0400

    Update VOD_Ad_Load.sql

--- VOD_Ad_Load.sql
+++ VOD_Ad_Load.sql
@@ -1,3 +1,5 @@
+CREATE OR REPLACE TABLE `nbcu-ds-sandbox-a-001.Shunchao_Sandbox.VOD_AD_LOAD_ALPHA_V1` as
+
 with SV as (select
 adobe_tracking_id,
 adobe_date,
@@ -8,9 +10,9 @@ when device_platform in ("Mobile App","Ios") then "Mobile"
 else "Others" end as Devices, -- break down by TV / Mobile / Others
 INITCAP(display_primary_genre) as Display_primary_genres,
 case when lower(regexp_extract(display_secondary_genre, r'[^;,&]+')) in ('comedy','drama','documentary') 
-     then lower(regexp_extract(display_secondary_genre, r'[^;,&]+')) 
-     else "others" end as Primary_Genre,
-case when lower(display_secondary_genre) like "%sport%" or lower(display_secondary_genre) like "%premier%league%" then "Sport" -- Incl. premier league to sport
+     then lower(regexp_extract(display_secondary_genre, r'[^;,&]+'))  
+     else "others" end as Primary_Genre, -- drama / comedy / documentary 
+case when lower(display_secondary_genre) like "%sport%" or lower(display_secondary_genre) like "%premier%league%" then "Sports" -- Incl. premier league to sport
      when lower(display_secondary_genre) like "%kids%" then "Kids"
      when lower(display_secondary_genre) like "%wwe%" then "WWE"
      Else 'Others' end as Secondary_Genre,
@@ -21,7 +23,7 @@ ad_id,
 num_seconds_played_with_ads,
 num_seconds_played_no_ads
 from `nbcu-ds-prod-001.PeacockDataMartSilver.SILVER_VIDEO`
-where adobe_date = "2023-04-01" and adobe_tracking_id is not null),
+where adobe_date between "2023-04-01" and "2023-04-26" and adobe_tracking_id is not null),
 
 SU as (
 select 
@@ -29,7 +31,7 @@ adobe_tracking_id,
 report_date,
 entitlement
 from `nbcu-ds-prod-001.PeacockDataMartSilver.SILVER_USER`
-where report_date = "2023-04-01" and entitlement = "Premium"
+where report_date between "2023-04-01" and "2023-04-26" and entitlement = "Premium"
 ),
 
 Combination as (select SV.*,
@@ -37,7 +39,7 @@ case
 when (lower(Display_primary_genres) = "tv" and Secondary_Genre = "WWE") or (lower(Devices) = "tv" and Secondary_Genre = "WWE") then "Tv-WWE" -- Display_Primary_Genre are all Sports for WWE, so I set up 2nd clause using devices
 when (lower(Display_primary_genres) = "tv" and Secondary_Genre = "Sports") then "Tv-Sports"
 when (lower(Display_primary_genres) = "tv" and Secondary_Genre = "Kids") then "Tv-Kids"
-when lower(Display_primary_genres) = "movies" and Secondary_Genre = "Kids" then "movies-kids"
+when lower(Display_primary_genres) = "movies" and Secondary_Genre = "Kids" then "Movies-kids"
 when (lower(Display_primary_genres) = "tv" and Secondary_Genre not in ("WWE","Sports","Kids")) or (lower(Devices) = "tv" and Secondary_Genre not in ("WWE","Sports","Kids")) then "Tv-originals"
 when lower(Display_primary_genres) = "movies" or Secondary_Genre != "Kids" then "Movies"
 else "Others" end as Content_Types,
@@ -58,7 +60,7 @@ round((sum(num_seconds_played_with_ads)- sum(num_seconds_played_no_ads))/60,0) a
 round(sum(ad_viewed)/24,0) as Ad_Unit_per_Hour,
 round((sum(num_seconds_played_with_ads)- sum(num_seconds_played_no_ads))/(60*24),0) as Ad_Minutes_Watched_per_Hour,
 round(sum(num_seconds_played_no_ads)/3600,2) as Hours_Watched,
-round(safe_divide(CAST(sum(ad_viewed) AS DECIMAL), CAST(count(ad_id) AS DECIMAL)),3) as Ad_Pods_Watched_Prc
+ifnull(round(safe_divide(CAST(sum(ad_viewed) AS DECIMAL), CAST(count(ad_id) AS DECIMAL)),3),0) as Ad_Pods_Watched_Prc
 from Combination 
 group by 1,2,3,4,5,6
 order by 1 desc
