7189b8a6db6215d3513c4260386ab9e4f639514e
ShunchaoChenNBC <117398619+ShunchaoChenNBC@users.noreply.github.com>
2023-05-03 12:41:42 -0400

    Update VOD_Ad_Load.sql

--- VOD_Ad_Load.sql
+++ VOD_Ad_Load.sql
@@ -45,9 +45,9 @@ when lower(Display_primary_genres) = "movies" or Secondary_Genre != "Kids" then 
 else "Others" end as Content_Types,
 SU.entitlement as Account_Entitlement
 from SV
-inner join SU on SV.adobe_date = SU.report_date and SV.adobe_tracking_id = SU.adobe_tracking_id) -- Only care about "Premium" tier
+inner join SU on SV.adobe_date = SU.report_date and SV.adobe_tracking_id = SU.adobe_tracking_id), -- Only care about "Premium" tier
 
-select 
+Alls as (select 
 adobe_date,
 Account_Entitlement,
 extract(week from adobe_date) as Week,
@@ -57,10 +57,13 @@ Content_Types,
 sum(num_views_started) as Content_Start,
 round(sum(ad_viewed),0) as Ad_Unit,
 round((sum(num_seconds_played_with_ads)- sum(num_seconds_played_no_ads))/60,0) as Ad_Minutes_Watched,
-round(sum(ad_viewed)/ (count(distinct adobe_tracking_id)*24),0) as Ad_Unit_per_Hour, -- “the average number of ad units that a viewer watched in a hour”
-round((sum(num_seconds_played_with_ads)- sum(num_seconds_played_no_ads))/(count(distinct adobe_tracking_id)*60*24),2) as Ad_Minutes_Watched_per_Hour,
 round(sum(num_seconds_played_no_ads)/3600,2) as Hours_Watched,
-ifnull(round(safe_divide(CAST(sum(ad_viewed) AS DECIMAL), CAST(count(ad_id) AS DECIMAL)),3),0) as Ad_Pods_Watched_Prc
+sum(ad_viewed) as Ad_Pods_Watched
 from Combination 
-group by 1,2,3,4,5,6
+group by 1,2,3,4,5,6)
+
+select a.*,
+ifnull(round(safe_divide(CAST(a.Ad_Unit AS DECIMAL), CAST(a.Hours_Watched AS DECIMAL)),2),0) as Ad_Unit_per_Hour,
+ifnull(round(safe_divide(CAST(a.Ad_Minutes_Watched AS DECIMAL), CAST(a.Hours_Watched AS DECIMAL)),2),0) as Ad_Minutes_Watched_per_Hour
+from Alls a
 order by 1 desc
