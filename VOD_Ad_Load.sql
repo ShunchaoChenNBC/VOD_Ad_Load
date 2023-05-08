56f62ad4488ec71543a09f66bb34884b9cc8c9c6
ShunchaoChenNBC <117398619+ShunchaoChenNBC@users.noreply.github.com>
2023-05-08 10:06:23 -0400

    Update VOD_Ad_Load.sql
    Add duration

--- VOD_Ad_Load.sql
+++ VOD_Ad_Load.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE TABLE `nbcu-ds-sandbox-a-001.Shunchao_Sandbox.VOD_AD_LOAD_ALPHA_V2` as
+CREATE OR REPLACE TABLE `nbcu-ds-sandbox-a-001.Shunchao_Sandbox.VOD_AD_LOAD_ALPHA_V3` as
 
 with SV as (select
 adobe_tracking_id,
@@ -16,12 +16,18 @@ case when lower(display_secondary_genre) like "%sport%" or lower(display_seconda
      when lower(display_secondary_genre) like "%kids%" then "Kids"
      when lower(display_secondary_genre) like "%wwe%" then "WWE"
      when lower(display_name) 
-     in (SELECT distinct lower(program) FROM `nbcu-ds-prod-001.PeacockDataMartMarketingGold.GOLD_ORIGINALS_PRIMARY_KPIS` where base_date between current_date("America/New_York")-5 and current_date("America/New_York")-1) then "Tv-original"
+     in (SELECT distinct lower(program) FROM `nbcu-ds-prod-001.PeacockDataMartMarketingGold.GOLD_ORIGINALS_PRIMARY_KPIS` where base_date between current_date("America/New_York")-5 and current_date("America/New_York")-1) then "Tv-original" -- recent Highlight Originals
      Else 'Others' end as Secondary_Genre,
 num_views_started,
 display_secondary_genre,
 ad_viewed,
 ad_id,
+case when promo_length < 15 then "<15S"
+     when  promo_length < 30 then "15-30S" 
+     when  promo_length < 60 then "30-60S"
+     else ">60S" end
+AS silver_Ad_Duration, --post_evar141 Creatibe Duration
+promo_video_position AS Ad_Pod_Name, --post_evar143
 num_seconds_played_with_ads,
 num_seconds_played_no_ads
 from `nbcu-ds-prod-001.PeacockDataMartSilver.SILVER_VIDEO`
@@ -43,8 +49,8 @@ when (lower(Display_primary_genres) in ("tv","Sports","News") and Secondary_Genr
 when (lower(Display_primary_genres) in ("tv","Sports","News") and Secondary_Genre = "Kids") then "Tv-Kids"
 when lower(Display_primary_genres) = "movies" and Secondary_Genre = "Kids" then "Movies-kids"
 when Secondary_Genre = "Tv-original" then "Tv-originals"
-when lower(Display_primary_genres) = "movies" or Secondary_Genre != "Kids" then "Movies"
-else "Others" end as Content_Types,
+when lower(Display_primary_genres) = "movies" and Secondary_Genre != "Kids" then "Movies"
+else "Tv-Others" end as Content_Types,
 SU.entitlement as Account_Entitlement
 from SV
 inner join SU on SV.adobe_date = SU.report_date and SV.adobe_tracking_id = SU.adobe_tracking_id), -- Only care about "Premium" tier
@@ -56,12 +62,14 @@ extract(week from adobe_date) as Week,
 Devices,
 Primary_Genre,
 Content_Types,
+silver_Ad_Duration, --Duration brkdwn
 sum(num_views_started) as Content_Start,
 round(sum(ad_viewed),0) as Ad_Unit,
 round((sum(num_seconds_played_with_ads)- sum(num_seconds_played_no_ads))/60,0) as Ad_Minutes_Watched,
 round(sum(num_seconds_played_no_ads)/3600,2) as Hours_Watched
 from Combination 
-group by 1,2,3,4,5,6)
+group by 1,2,3,4,5,6,7
+)
 
 select a.*,
 ifnull(round(safe_divide(CAST(a.Ad_Unit AS DECIMAL), CAST(a.Hours_Watched AS DECIMAL)),2),0) as Ad_Unit_per_Hour,
