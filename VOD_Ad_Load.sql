361d676d22ad50026df4b5a83e9a41f036f6aa09
ShunchaoChenNBC <117398619+ShunchaoChenNBC@users.noreply.github.com>
2023-05-11 10:07:28 -0400

    Update VOD_Ad_Load.sql
    Change WWE classification logic
    Ad time watched logic
    Ad duration logic

--- VOD_Ad_Load.sql
+++ VOD_Ad_Load.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE TABLE `nbcu-ds-sandbox-a-001.Shunchao_Sandbox.VOD_AD_LOAD_Beta` as
+CREATE OR REPLACE TABLE `nbcu-ds-sandbox-a-001.Shunchao_Sandbox.VOD_AD_LOAD_Beta_V1` as
 
 with SV as (select
 adobe_tracking_id,
@@ -9,9 +9,7 @@ when device_name in ("Ios Mobile", "Amazon Fire Tablet", "Android Mobile") then 
 when device_name = "Www" then "Web"
 else "TV" end as Devices, -- break down by TV / Mobile / Web
 INITCAP(display_primary_genre) as Display_primary_genres,
-case when lower(regexp_extract(display_secondary_genre, r'[^;,&]+')) in ('comedy','drama','documentary') 
-     then lower(regexp_extract(display_secondary_genre, r'[^;,&]+'))  
-     else "others" end as Primary_Genre, -- drama / comedy / documentary 
+lower(regexp_extract(display_secondary_genre, r'[^;,&]+')) as Primary_Genre, -- first entry before semi-commas
 case when lower(display_secondary_genre) like "%sport%" or lower(display_secondary_genre) like "%premier%league%" then "Sports" -- Incl. premier league to sport
      when lower(display_secondary_genre) like "%kids%" then "Kids"
      when lower(display_secondary_genre) like "%wwe%" then "WWE"
@@ -20,20 +18,24 @@ case when lower(display_secondary_genre) like "%sport%" or lower(display_seconda
      Else 'Others' end as Secondary_Genre,
 num_views_started,
 display_secondary_genre,
+franchise,
 ad_viewed,
 ad_served,
 promo_length,
-case when promo_length < 15 then "<15S"
-     when  promo_length < 30 then "15-30S" 
-     when  promo_length < 60 then "30-60S"
-     when  promo_length >= 60 then ">60S"
-     else "" end -- Added logic to deal with null value
+case when promo_length < 12 then "<12S"
+     when  promo_length <= 20 then "15S" 
+     when  promo_length <= 40 then "30S"
+     when  promo_length <= 65 then "60S"
+     when  promo_length <= 80 then "75S"
+     when  promo_length <= 95 then "90S"
+     when  promo_length > 95 then ">95"    
+     else "" end
 AS silver_Ad_Duration, --post_evar141 Creatibe Duration
 promo_video_position AS Ad_Pod_Name, --post_evar143
 num_seconds_played_with_ads,
 num_seconds_played_no_ads
 from `nbcu-ds-prod-001.PeacockDataMartSilver.SILVER_VIDEO`
-where adobe_date between "2023-01-01" and "2023-05-08" and adobe_tracking_id is not null and lower(consumption_type_detail) = "vod"),
+where adobe_date between "2023-04-01" and "2023-04-10" and adobe_tracking_id is not null and lower(consumption_type_detail) = "vod"),
 
 SU as (
 select 
@@ -41,12 +43,12 @@ adobe_tracking_id,
 report_date,
 entitlement
 from `nbcu-ds-prod-001.PeacockDataMartSilver.SILVER_USER`
-where report_date between "2023-01-01" and "2023-05-08" and entitlement = "Premium"
+where report_date between "2023-04-01" and "2023-04-10" and entitlement = "Premium"
 ),
 
 Combination as (select SV.*,
 case 
-when (lower(Display_primary_genres) in ("tv","Sports","News") and Secondary_Genre = "WWE") or (lower(Devices) = "tv" and Secondary_Genre = "WWE") then "Tv-WWE" -- Display_Primary_Genre are all Sports for WWE, so I set up 2nd clause using devices
+when (lower(Display_primary_genres) in ("tv","Sports","News") and lower(franchise) = "wwe") then "Tv-WWE" -- Display_Primary_Genre are all Sports for WWE, so I set up 2nd clause using devices
 when (lower(Display_primary_genres) in ("tv","Sports","News") and Secondary_Genre = "Sports") then "Tv-Sports"
 when (lower(Display_primary_genres) in ("tv","Sports","News") and Secondary_Genre = "Kids") then "Tv-Kids"
 when lower(Display_primary_genres) = "movies" and Secondary_Genre = "Kids" then "Movies-kids"
@@ -54,7 +56,7 @@ when Secondary_Genre = "Tv-original" then "Tv-originals"
 when lower(Display_primary_genres) = "movies" and Secondary_Genre != "Kids" then "Movies"
 else "Tv-Others" end as Content_Types,
 SU.entitlement as Account_Entitlement,
-case when ad_served = 1 then promo_length else 0 end as Ad_Time_Watched -- ad_served (Ad completed) then ad watched, so ad length is ad time
+num_seconds_played_with_ads - num_seconds_played_no_ads as Ad_Time_Watched -- with_ad minus without ad to get ad time watched
 from SV
 inner join SU on SV.adobe_date = SU.report_date and SV.adobe_tracking_id = SU.adobe_tracking_id), -- Only care about "Premium" tier
 
@@ -66,12 +68,13 @@ Devices,
 Primary_Genre,
 Content_Types,
 silver_Ad_Duration, --Duration brkdwn
+Ad_Pod_Name,
 sum(num_views_started) as Content_Start,
 round(sum(ad_viewed),0) as Ad_Unit,
 round(sum(Ad_Time_Watched)/60,0) as Ad_Minutes_Watched,
 round(sum(num_seconds_played_no_ads)/3600,2) as Hours_Watched
 from Combination 
-group by 1,2,3,4,5,6,7
+group by 1,2,3,4,5,6,7,8
 )
 
 select a.*,
