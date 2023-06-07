1da201848fb9052e26c3a1e6b5ef9fe10343c132
ShunchaoChenNBC <117398619+ShunchaoChenNBC@users.noreply.github.com>
2023-06-06 20:54:06 -0400

    Update VOD_AD_LOAD_Data_Raw_w_Duration.sql

--- VOD_AD_LOAD_Data_Raw_w_Duration.sql
+++ VOD_AD_LOAD_Data_Raw_w_Duration.sql
@@ -74,6 +74,9 @@ from  `nbcu-ds-sandbox-a-001.Shunchao_Sandbox.VOD_AD_LOAD_Data_Raw_w_Duration`
 
 --------------------------------------------------------------------------- Aggregation ----------------------------------------------------
 
+create or replace table `nbcu-ds-sandbox-a-001.Shunchao_Sandbox.VOD_AD_LOAD_Table_For_Dash` as 
+
+
 select 
 adobe_date,
 Account_Entitlement,
@@ -82,14 +85,13 @@ set_duration,
 Devices,
 trim(Primary_Genre) as Primary_Genre, -- remove whitespace
 Content_Types,
-silver_Ad_Duration, --Duration brkdwn
 Ad_Pod_Name,
 sum(num_views_started) as Content_Start,
 round(sum(ad_viewed),0) as Ad_Unit,
 round(sum(Ad_Time_Watched)/60,0) as Ad_Minutes_Watched,
 round(sum(num_seconds_played_no_ads)/3600,2) as Hours_Watched
 from 
- `nbcu-ds-sandbox-a-001.Shunchao_Sandbox.VOD_AD_LOAD_Data_Raw_w_Duration` 
-group by 1,2,3,4,5,6,7,8,9
+`nbcu-ds-sandbox-a-001.Shunchao_Sandbox.VOD_AD_LOAD_Table` 
+group by 1,2,3,4,5,6,7,8
 
 
