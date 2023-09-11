f73394427335533399d97787c8adead0e69a42fd
ShunchaoChenNBC <117398619+ShunchaoChenNBC@users.noreply.github.com>
2023-09-11 16:56:13 -0400

    Update VOD_AD_LOAD_Data_P1&P2.sql
    New formula: Ad min per hour = Ad min watched / Ad min watched + content time

--- VOD_AD_LOAD_Data_P1&P2.sql
+++ VOD_AD_LOAD_Data_P1&P2.sql
@@ -89,7 +89,7 @@ Ad_Pod_Name,
 sum(num_views_started) as Content_Start,
 round(sum(ad_viewed),0) as Ad_Unit,
 round(sum(Ad_Time_Watched)/60,0) as Ad_Minutes_Watched,
-round(sum(num_seconds_played_no_ads)/3600,2) as Hours_Watched
+round(sum(num_seconds_played_with_ads)/3600,2) as Hours_Watched
 from 
 `nbcu-ds-sandbox-a-001.Shunchao_Sandbox.VOD_AD_LOAD_Table` 
 group by 1,2,3,4,5,6,7,8
